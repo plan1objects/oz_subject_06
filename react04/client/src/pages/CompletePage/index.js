e9791552b03b5ab4ce15e1cae19b016de6215b99
plan1objects <plan1object@gmail.com>
2024-02-24 18:58:38 +0900

    challenge04

--- react04/client/src/pages/CompletePage/index.js
+++ react04/client/src/pages/CompletePage/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { OrderContext } from '../../context/OrderContext';
+import axios from 'axios';
 
 const CompletePage = () => {
+
+    const [orderHistory, setOrderHistory] = useState([]);
+    const [orderData] = useContext(OrderContext);
+
+    useEffect(() => {
+        orderCompleted(orderData);
+    },[orderData]);
+
+    const orderCompleted = async (orderData) => {
+        try{
+            const response = await axios.post('http://localhost:4000/order', orderData);
+            setOrderHistory(response.data);
+        }catch(error){
+            console.error(error);
+        }
+    }
+    
+
     return (
         <div>
-            
+            CompletePage
         </div>
     );
 };
